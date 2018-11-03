31667f659148eb2152173e3c4152e6f4283bf741
armouti <alarmouti.ismail@gmail.com>
2018-11-03 13:59:56 -0600

    Devided the header area using flex. Setup and positioned some elements in CourseHeader

--- client/src/components/CourseHeader.js
+++ client/src/components/CourseHeader.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
-
 class CourseHeader extends Component {
 
   render() {
     return (
       <div style={styles.headerContainerStyle}>
+        <div style={styles.leftContainerStyle}>
+          <h1>{'Learn This Now'}</h1>
+          <h4>{'This course is all about honing your experience to become whatever you want to be.'}</h4>
+        </div>
+        <div style={styles.rightContainerStyle}>
+          {'section 2'}
+        </div>
       </div>
     );
   }
@@ -16,7 +22,21 @@ const styles = {
     height: '52%',
     width: '100%',
     backgroundColor: '#505763',
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  leftContainerStyle:{
+    backgroundColor: 'blue',
+    flex: 3.5,
+    paddingTop: '4.5%',
+    paddingLeft: '5%'
+  },
+  rightContainerStyle: {
+    backgroundColor: 'red',
+    paddingTop: '4.5%',
+    flex: 2
   }
+
 }
 export default CourseHeader;
 
--- client/src/components/CoursePage.js
+++ client/src/components/CoursePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import CourseHeader from './CourseHeader.js';
+import { Breadcrumb } from 'semantic-ui-react';
 
 class CoursePage extends Component {
   constructor(props){
@@ -9,6 +10,13 @@ class CoursePage extends Component {
   render() {
     return (
       <div style={styles.pageContainerStyle}>
+        <Breadcrumb style={styles.breadcrumbStyle} size='small'>
+          <Breadcrumb.Section style={styles.crumbStyle}>{'Business'}</Breadcrumb.Section>
+          <Breadcrumb.Divider icon='right chevron' />
+          <Breadcrumb.Section style={styles.crumbStyle}>{'Entrepreneurship'}</Breadcrumb.Section>
+          <Breadcrumb.Divider icon='right chevron' />
+          <Breadcrumb.Section active style={styles.crumbStyle}>{'One Day MVP'}</Breadcrumb.Section>
+        </Breadcrumb>
         <CourseHeader courseData={this.props.courseData}/>
       </div>
     );
@@ -16,7 +24,8 @@ class CoursePage extends Component {
 }
 
 const styles = {
-  pageContainerStyle: { marginTop: '50px' }
+  pageContainerStyle: {},
+  breadcrumbStyle: { paddingLeft: '4%', marginBottom: '1%', width:'100%' },
+  crumbStyle: { marginLeft: '1%', marginRight: '1%' }
 }
-
 export default CoursePage;
