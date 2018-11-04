1c642af9a78f7ec3de7a524aabfc6366ced198ca
armouti <alarmouti.ismail@gmail.com>
2018-11-03 19:43:32 -0600

    created the GET /course api endpoint

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const Models = require('../db/Models.js');
+const { Course } = require('../db/Models.js');
 
 const app = express();
 
@@ -14,7 +14,10 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.get('/', (req, res) => res.render('index.html'));
 
 app.get('/course', (req, res) => {
-// return course requested in req
+  Course.findCourseById(2/*req.body.courseId*/)
+  .then((course) => {
+    res.send(course);
+  })
 });
 
 app.listen(port, () => console.log('listening on port ' + port));
