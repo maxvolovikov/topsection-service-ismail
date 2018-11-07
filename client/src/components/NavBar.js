4bc55e006f4375e04bef0348acaebe5b7c1394c1
armouti <alarmouti.ismail@gmail.com>
2018-11-06 20:56:46 -0600

    added and styled the LogIn and SignUp buttons in the NavBar component.

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Menu, Segment, Icon, Input, Breadcrumb } from 'semantic-ui-react';
+import { Menu, Segment, Icon, Input, Breadcrumb, Button } from 'semantic-ui-react';
 
 class NavBar extends Component {
 
@@ -11,26 +11,33 @@ class NavBar extends Component {
             <Menu.Item name='logo'>
               <img style={styles.logoStyle} src='https://www.udemy.com/staticx/udemy/images/v6/logo-coral.svg' />
             </Menu.Item>
-            <Menu.Item name='categories'>
+            <Menu.Item
+              style={{ color:'#505763', fontFamily: 'Open Sans,Helvetica Neue,Helvetica,Arial,sans-serif', fontSize: '13px', fontWeight: 400 }}
+              name='categories'
+            >
               <Icon name='th' /> {'Categories'}
             </Menu.Item>
             <Menu.Item name='searchBar' style={styles.inputContainerStyle}>
               <Input style={styles.inputStyle} action={{ icon: 'search' }} placeholder='Search for Courses'/>
             </Menu.Item>
             <Menu.Item
+              style={styles.navBtnStyle}
               name='Udemy For Business'
               active
             />
             <Menu.Item
               name='Become an Instructor'
+              style={styles.navBtnStyle}
             />
             <Menu.Item
               style={styles.cardItemStyle}
-              name='Become an Instructor'
             >
               <Icon size='large' name='shopping cart' />
             </Menu.Item>
-
+            <Menu.Item style={{ paddingTop: 0, paddingBottom: 0 }}>
+              <Button style={styles.logInBtnStyle} basic> {'Log In'}</Button>
+              <Button style={styles.signUpBtnStyle}> {'Sign Up'}</Button>
+            </Menu.Item>
           </Menu>
         </Segment>
       </div>
@@ -38,26 +45,46 @@ class NavBar extends Component {
   }
 }
   const styles= {
+    segmentStyle: {
+      paddingTop: '5px',
+      paddingBottom: '5px',
+      paddingRight: '60px',
+      boxShadow: '0 0 1px 1px rgba(20,23,28,.1), 0 3px 1px 0 rgba(20,23,28,.1)'
+    },
     logoStyle: { zoom: 3 },
-    segmentStyle: { paddingTop: '5px', paddingBottom: '5px'},
-    inputContainerStyle: { width: '35%', marginLeft: '1%', marginRight: '3%'},
+    inputContainerStyle: { width: '32%', marginLeft: '1%', marginRight: '2%'},
     inputStyle: {
       width: '100%',
       height: '40px',
       borderRadius: 0
     },
-    cartItemStyle: {
-      borderRadius: 1000
+    navBtnStyle: {
+      fontFamily: 'Open Sans,Helvetica Neue,Helvetica,Arial,sans-serif' ,
+      fontSize: '13px',
+      fontFamily: 400,
+      lineHeight: '20px',
+      color: '#505763',
     },
-    cartIconStyle: {
-      borderRadius: 1000
+    cardItemStyle: {
+      paddingLeft: '2px',
+      paddingRight: '2px'
+    },
+    logInBtnStyle: {
+      borderRadius: '2px',
+      marginRight: '5px',
+      border: '1px solid #686f7a',
+      fontSize: 15,
+      fontWeight: 600
+    },
+    signUpBtnStyle: {
+      backgroundColor: '#ec5252',
+      color: '#fff',
+      fontSize: 15,
+      fontWeight: 600,
+      borderRadius: '2px',
+      border: '1px solid #ec5252'
     }
   }
-  // <Breadcrumb size='small'>
-  //   <Breadcrumb.Section link>Home</Breadcrumb.Section>
-  //   <Breadcrumb.Divider icon='right chevron' />
-  //   <Breadcrumb.Section link>Registration</Breadcrumb.Section>
-  //   <Breadcrumb.Divider icon='right chevron' />
-  //   <Breadcrumb.Section active>Personal Information</Breadcrumb.Section>
-  // </Breadcrumb>
+
 export default NavBar;
+
