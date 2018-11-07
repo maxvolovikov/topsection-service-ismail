3ec3225041f1f2cf72726506bde9b5267510d6bc
armouti <alarmouti.ismail@gmail.com>
2018-11-07 16:47:58 -0600

    added fetch call to retreive course data from the api. added state tracking in coursePage component to hold the data for the fetched course. Changed CourseHeader component in the coursePage component-tree to recieve its courseData from the local state.

--- client/src/components/CoursePage.js
+++ client/src/components/CoursePage.js
@@ -4,6 +4,19 @@ import CourseHeader from './CourseHeader.js';
 class CoursePage extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      course: {}
+    };
+  }
+
+  componentDidMount(){
+    const courseId = Math.floor(Math.random() * 20000);
+    fetch(`/course/${courseId}`)
+    .then((raw) => raw.json())
+    .then((course) => {
+      console.log('course', course);
+      this.setState({ course });
+    });
   }
 
   render() {
